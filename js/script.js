8bc24b5c46bf06cc948deefae3939cf69ec2cdf4
Zoey <b00408044@g.ntu.edu.tw>
2019-01-15 13:09:41 +0800

    Edited the skill section

--- js/script.js
+++ js/script.js
@@ -36,6 +36,8 @@ $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
         loop: true,
         items: 4,
+        nav: true,
+        autoplay: true,
         responsive: {
             0: {
                 items: 1
